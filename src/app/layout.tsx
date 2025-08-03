e752d855def4162a35937c70aafb24bb7c457420
Emir Ale <emirderespaldo@gmail.com>
2025-08-03 16:10:42 +0000

    I'm still not happy about the white background, add a vecorize unneven p

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('min-h-screen bg-background font-body antialiased animated-background')}>
+      <body className={cn('min-h-screen bg-background font-body antialiased')}>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
