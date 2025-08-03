dda0526ffbb2908f2d243d11afc92d7d58a99535
Emir Ale <emirderespaldo@gmail.com>
2025-08-03 02:39:41 +0000

    I need you to do something with the background, make it more alive, use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('min-h-screen bg-background font-body antialiased')}>
+      <body className={cn('min-h-screen bg-background font-body antialiased animated-background')}>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
@@ -32,7 +32,7 @@ export default function RootLayout({
             disableTransitionOnChange
         >
             <CartProvider>
-              <div className="relative flex min-h-dvh flex-col">
+              <div className="relative flex min-h-dvh flex-col bg-background/80 backdrop-blur-sm">
                 <Header />
                 <main className="flex-1 container py-8">
                   {children}
